764fe4fbcf71fc49b33089796c408103ee1ba9a2
michalziebaa <michal.ziebaa@gmail.com>
2020-12-12 18:46:30 +0100

    add download_video fun

---
+++ download_video.py
@@ -0,0 +1,14 @@
+import pytube
+from moviepy.editor import *
+import os
+
+url = 'https://www.youtube.com/watch?v=Cpdw4mVSJdc'
+
+def download_video(url):
+    youtube = pytube.YouTube(url)
+    video = youtube.streams.first()
+    try:
+        video.download()
+        print('[+] Downloaded!')
+    except:
+        print('[-] Something went wrong...')
