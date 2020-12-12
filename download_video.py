a5ceb4d2dc7700a13802fb6d51cc3bac8dba42dd
michalziebaa <michal.ziebaa@gmail.com>
2020-12-13 00:45:35 +0100

    added bookmarks download ficzer

--- download_video.py
+++ download_video.py
@@ -1,12 +1,20 @@
 import pytube
 from moviepy.editor import *
 import os
+import chrome_bookmarks
 
 url = 'https://www.youtube.com/watch?v=Cpdw4mVSJdc'
 #(song_url TEXT, path TEXT, title TEXT)
-# 
 
+def urls_in_folder(folder, url_list):
+    for url in folder.urls:
+        #print(url['url'], end=' ')
+        #print(url['name'])
+        url_list.append(url['url'])
 
+    for child in folder.folders:
+        urls_in_folder(child, url_list)
+    
 def download_video(url):
     youtube = pytube.YouTube(url)
 
@@ -46,4 +54,15 @@ def download_video_as_mp3(url):
     except:
         print('[-] Something went wrong...')
 
+
+def download_from_bookmarks(bookmark_name):
+    url_list = []
+    tuple_list = []
+    for folder in chrome_bookmarks.folders:
+        if folder.name == bookmark_name:
+            urls_in_folder(folder, url_list)
+
+    for u in url_list:
+        tuple_list.append(download_video_as_mp3(u))
+    return tuple_list
 
