3e33d2d1525ed2ebd1726a3003c41895f0c9790e
piotr bosowski <piotr.bosowski@gmail.com>
2021-01-30 20:00:55 +0100

    Fix settings.py typo

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # saved songs will be stored here:
-save_audio_path = r'././data/audio'
+save_audio_path = r'./.data/audio'
 
 # database with songs info will be saved under:
 database_path = './.data/database'
